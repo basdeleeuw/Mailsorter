55499f7d722b435e675fd63c1da7c51859aba764
Bas de Leeuw <basj.dl@gmail.com>
2019-07-24 13:02:27 +0200

    Typo's verbeterd en readme verwijderd

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ SAVEPATH = "./mail.oft"
 HEADERLIST_END = "==END=="
 NOREPORT_HEADERS = [
     4, 5, 6
-]  #Headers to be neglected. Note that the counting starts with 0, so the 4th header is actually number 3 here!
+]  # Headers to be neglected. Note that the counting starts with 0, so the 4th header is actually number 3 here!
 
 
-#Leest de headers in uit HEADERPATH en returnt ze als een matrix[headernr][regelnr]
-#OUT:   headers: een lijst met de headers van de errors in een [headernumber][2]-matrix
+# Reads the headers from HEADERPATH and returns them as a matrix[headernr][linenr]
+# OUT:   headers: a list ot the error-headers as a [headernr][2]-matrix
 def readheaders():
     headerfile = open(HEADERPATH, 'r')
     headers = []
@@ -28,10 +28,10 @@ def readheaders():
     return headers
 
 
-#Leest de daadwerkelijke foutmeldingen uit een mailtje en returnt ze als een matrix[headernr][regelnr]
-#IN:    filename: The path to the file from which the errors are to be extracted
-#       headerlist: A list of the error headers in a [headernumber][2]-matrix
-#OUT:   errors: a matrix[headernumber][linenumber] of the errors in the e-mail
+# Reads the actual errors from an e-mail and returns them as a matrix[headernr][linenr]
+# IN:    filename: The path to the file from which the errors are to be extracted
+#        headerlist: A list of the error headers in a [headernr][2]-matrix
+# OUT:   errors: a matrix[headernr][linenr] of the errors in the e-mail
 def geterrors(filename, headerlist):
     file = open(MAILPATH + "/" + filename, 'r')
     errors = [[] for x in range(len(headerlist))]
@@ -55,7 +55,7 @@ def geterrors(filename, headerlist):
     return errors
 
 
-#Checkt of er errors zijn die gereport moeten worden
+# Checks of there are errors to be reported
 def checkerrors(errors):
     reportable_errors = False
     for i in range(len(errors)):
@@ -64,16 +64,16 @@ def checkerrors(errors):
     return reportable_errors
 
 
-#Schrijft de errors naar een bestand dat kan worden gecopypasted in de mail
-#IN:    filelist: A list of the file names of the errorlogs
-#       errorlist: A matrix[headernumber][linenumber] of the errors in the e-mail
-#       headerlist: A list of the error headers in a [headernumber][2]-matrix
+# Writes the errors to an e-mail
+# IN:    filelist: A list of the file names of the errorlogs
+#       errorlist: A matrix[headernr][linenr] of the errors in the e-mail
+#       headerlist: A list of the error headers in a [headernr][2]-matrix
 def save_errors(filelist, errorlist, headerlist):
     print("You forgot to implement the save_errors routine you dummy")
 
 
-#Print de errors een beetje leesbaar uit, vooral voor troubleshooting
-#IN:    errors: a matrix[headernumber][linenumber] of the errors in the e-mail
+# Prints the errors to the screen in a semi-readable way, mainly for debugging
+# IN:    errors: a matrix[headernr][linenr] of the errors in the e-mail
 def printerrors(errors):
     for i in range(len(errors)):
         for j in range(len(errors[i])):
@@ -89,7 +89,7 @@ errors = []
 
 for i in range(len(files)):
     errors.append(geterrors(files[i], headers))
-    #printerrors(errorslist[i])
+    # printerrors(errorslist[i])
 
 save_errors(files, errors, headers)
 outlook = win32.Dispatch('outlook.application')
